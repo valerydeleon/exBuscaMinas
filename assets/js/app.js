c287262da76840d0339d381f3b980d87a0ab7f21
Valeria de Leon <valery.deleon@gmail.com>
2017-04-05 08:56:51 -0500

    estructura, sin funcionalidad

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+// hacer tabla con cuadros a clikear
+// debe tener un frente y un detras
+// al hacer click se debe descubrir  uncolor o un numero una bomba
+// para descubrir un elemento se puede crear
+//
+////////////// ******* No lo logré y no quizé copiar, me sentia muy mal ayer  ****** //////////
+
+var arregloVacio =[]
+espacios
+
+var obtenerClase = document.getElementsByClassName("CuadroVacio");
+obtenerClase.addEventListener("click", )
+
+
+// function clickBoton(){
+//   alert("click");
+//
+//   var contenedorDeBombas = document.getElementById("contenedorDeBombas");
+//
+//   var trtabla = createElement("tr")
+//   td
+// }
+//
+// v
+//
+// contenedorDeBombas.appendChild(trtabla);
+// contenedorDeBombas.addEventListener('click', clickBoton)
